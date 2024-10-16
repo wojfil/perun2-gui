caf9d6d7fdd273605f474d20fd794478d08dad6b
wojfil <wojfil@protonmail.com>
2024-10-16 20:47:22 +0200

    Mark version 0.8.9

--- WindowsOS/Gui/Perun2Gui/Properties/AssemblyInfo.cs
+++ WindowsOS/Gui/Perun2Gui/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.8.1")]
-[assembly: AssemblyFileVersion("0.8.8.1")]
+[assembly: AssemblyVersion("0.8.9.0")]
+[assembly: AssemblyFileVersion("0.8.9.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
