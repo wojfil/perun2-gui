a299cb7f9c65bc3b84e1e0411f6e402019bd46c8
wojfil <wojfil@protonmail.com>
2025-05-14 00:17:46 +0200

    Logs selection state

--- WindowsOS/Gui/Perun2Gui/MainForm_Log.cs
+++ WindowsOS/Gui/Perun2Gui/MainForm_Log.cs
@@ -15,9 +15,54 @@ namespace Perun2Gui
         {
             copyToolStripMenuItem_log.Enabled = ResizeEnabled && logBox.SelectionLength != 0;
             copyAllToolStripMenuItem.Enabled = ResizeEnabled && logBox.Text.Any();
+
+            LogsSelectionState state = GetLogSelectionState();
+
+            switch (state)
+            {
+                case LogsSelectionState.None: 
+                {
+                    showThemToolStripMenuItem.Text = "Show them in Explorer";
+                    openThemToolStripMenuItem.Text = "Open them";
+                    showThemToolStripMenuItem.Enabled = false;
+                    openThemToolStripMenuItem.Enabled = false;
+                    break;
+                }
+                case LogsSelectionState.OneFile:
+                {
+                    showThemToolStripMenuItem.Text = "Show it in Explorer";
+                    openThemToolStripMenuItem.Text = "Open it";
+                    showThemToolStripMenuItem.Enabled = true;
+                    openThemToolStripMenuItem.Enabled = true;
+                    break;
+                }
+                case LogsSelectionState.ManyFiles:
+                {
+                    showThemToolStripMenuItem.Text = "Show them in Explorer";
+                    openThemToolStripMenuItem.Text = "Open them";
+                    showThemToolStripMenuItem.Enabled = true;
+                    openThemToolStripMenuItem.Enabled = true;
+                    break;
+                }
+            }
         }
 
+        private LogsSelectionState GetLogSelectionState()
+        {
+            if (logBox.SelectionLength == 0)
+            {
+                return LogsSelectionState.None;
+            }
 
+            string[] lines = logBox.SelectedText.Split(new[] { "\r\n", "\n" }, 
+                StringSplitOptions.None);
+
+            // todo
+
+
+
+            return LogsSelectionState.None;
+        }
 
     }
 }
